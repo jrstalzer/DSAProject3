1ce164325d6a6b69074135f333b8c2ed91446957
Anamaria Cotelo <acotelolarrea@ufl.edu>
2022-12-05 18:22:31 -0500

    pretty much main

--- main.cpp
+++ main.cpp
@@ -3,19 +3,22 @@
 #include <fstream>
 #include <sstream>
 #include <string>
+#include <set>
 #include <iomanip>
+#include <ctime>
+#include "Hash.h"
 using namespace std;
 
 int main() {
+    clock_t runTime;
     ifstream reviewFile("amazon_reviews.csv");
-
+    set<string> categories;
+    Hash table;
     if (reviewFile.is_open()) {
         string line;
         getline(reviewFile, line);
 
-        int i = 0;
-        while (i < 2) {
-            getline(reviewFile, line);
+        while (getline(reviewFile, line)) {
             istringstream stream(line);
             string toss;
             string name;
@@ -23,25 +26,74 @@ int main() {
             string tempRate;
             double rating = 0;
 
-            getline(stream, toss, ','); //toss username
-            getline(stream, toss, '"'); //toss verified status
-            getline(stream, name, '"'); // keep item name, uses a dif delimeter because name can have commas
-            getline(stream, toss, ']'); //toss description
-            getline(stream, toss, ']'); //toss image
-            getline(stream, toss, ','); //toss filler
-            getline(stream, toss, ','); //toss feature
-            getline(stream, toss, ','); //toss brand
-            getline(stream, category, ','); //keep category
-            getline(stream, toss, ','); //toss price
-            getline(stream, tempRate, ','); //keep rating
-            rating = stod(tempRate);
 
-            i++;
-            cout << name << " " << category << " " << rating << endl;
+            char first = line.at(0);
+            char second = '"';
+            if (first == second) {
+                getline(stream, toss, '"'); //toss brand
+                getline(stream, name, '"'); // keep item name, uses a dif delimeter because name can have commas
+                getline(stream, toss, ','); //toss brand
+                getline(stream, category, ','); //keep category
+                getline(stream, tempRate, ','); //keep rating
+                rating = stod(tempRate);
+            }
+            else {
+                getline(stream, name, ','); // keep item name, uses a dif delimeter because name can have commas
+                getline(stream, category, ','); //keep category
+                getline(stream, tempRate, ','); //keep rating
+                rating = stod(tempRate);
+            }
+
+            categories.insert(category); // this was used to test data reading
+            //add insert method calls here
+            table.UpdateCategory(category, name, rating);
         }
 
     }
-    else
-        cout << "error :(" << endl;
+    else {
+        cout << "error reading file :(" << endl;
+    }
+    //now that data is in structures, which structure do we use?
+    int choice;
+    cout << "What data structure would you like to use today?" << endl;
+    cout << "1. Hash Table" << endl << "2. 5-ary tree" << endl;
+    cin >> choice;
+    cout << "Please select the category number you would like to search" << endl;
+    int i = 1;
+    for (auto iter = categories.begin(); iter != categories.end(); iter++) {
+        cout << i << ": " << *iter << endl;
+        i++;
+    }
+    int search;
+    cin >> search;
+    string searchFor = "";
+    auto iter = categories.begin();
+    i = 0;
+    for (auto iter = categories.begin(); iter != categories.end(); iter++) {
+        i++;
+        if (i == search) {
+            searchFor = *iter;
+        }
+    }
+    //cout << searchFor << endl;
+    switch (choice) {
+    case 1:
+        //hast table
+        runTime = clock(); //start of searching
+        //search functions
+        table.PrintHighestLowestForCategory(searchFor);
+        runTime = clock() - runTime;
+        cout << "It took a hash table " << runTime / CLOCKS_PER_SEC << " seconds to run!" << endl;
+        break;
+    case 2:
+        //5-ary
+        runTime = clock(); //start of searching
+        //search functions
+        runTime = clock() - runTime;
+        cout << "It took a 5-ary " << runTime / CLOCKS_PER_SEC << " seconds to run!" << endl;
+        break;
+    }
+
+
     return 0;
 }
